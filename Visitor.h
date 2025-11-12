6601998863e1d717e919eae57a0d6a1d8f5243bc
emadzakaria <emad.zakaria@student.giu-uni.de>
2025-11-12 15:15:04 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -5,6 +5,28 @@
 using namespace std;
 
 //define visitor class here
+#inlude <iostream>
+using namespace std;
+
+
+private class Visitor {
+    private
+    string visitorname ;
+    int ticketsBought;
+    
+    
+}
+private :
+Visitor (string visitorname, int ticketsBought);
+visitorname= Vs;
+ticketsBought=ts;
+Visitor();
+
+
+
+void dipslayinfo():
+cout<< "name= "<< visitorname << endl;
+cout<<"ticket = "<<ticketsBought<< endl;
 
 
 
