cbfb2dc63f2b81c7e615ff15cb6c4f4bd94cc3d0
emadzakaria <emad.zakaria@student.giu-uni.de>
2025-11-12 15:16:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,59 @@
 #include <iostream>
 using namespace std;
 
+#include "Animal.h";
+#include "Mammal.h";
+#include <iostream>
+using namespace std;
+Animal :: Animal (string name, int age, bool isHungry);
+this->name = name;
+this-> age = agel;
+this-> isHungry = NULL;
+
+    
+}
+
+Mammal::Mammal (string furColor);
+this-> furColor = furColor;
+
+}
+
+
+Bird::Bird (float wingSpan);
+this-> wingSpan = wingSpan;
+
+}
+
+
+Reptile::Reptile (bool isVenomous);
+this-> isVenomous= NULL;
+
+}
+
+
+
+Enclosure::Enlcosure (string name, int capacity, int currentCount);
+this-> name= name;
+this-> capacity = capacity;
+this-> currentCount = currentCount;
+   )}
+   
+  Visitor::Visitor (string visitorname, int ticketsBought);
+  this-> visitorname= visitorname;
+  this-> ticketsBought = ticketsBought;
+  
+
+
+int main()
+{
+   cout<<:"Enclosure = " <<
+   
+
+    return 0;
+}
+
+
+
 
 // ============== MAIN FUNCTION ==============
 int main() {
