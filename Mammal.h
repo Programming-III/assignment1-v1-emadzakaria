cb76d7aa58b3ad4540f52bb9656c395804280c7e
emadzakaria <emad.zakaria@student.giu-uni.de>
2025-11-12 15:14:05 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -2,6 +2,19 @@
 #define MAMMAL_H
 
 #include "Animal.h"
+#inlude <iostream>
+using namespace std;
+#ifndef  Anmial _h 
+define Anmial_h
+
+private class Mammal {
+    private :
+    string furColor;
+}
+private:
+Mammal (string furColor);
+furColor= FC;
+Mammal();
 
 //define mammal class here
 
