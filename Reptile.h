33914cacf7e9aeda1aa9c591efb318ba08294fb1
emadzakaria <emad.zakaria@student.giu-uni.de>
2025-11-12 15:14:32 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -4,7 +4,19 @@
 #include "Animal.h"
 
 //define reptile class here
+#inlude <iostream>
+using namespace std;
+#ifndef  Anmial _h 
+define Anmial_h
 
+private class Reptile {
+    private :
+   bool isVenomous;
+}
+private:
+Reptile (bool isVenomous);
+isVenomous=IV;
+Reptile();
 
 
 
